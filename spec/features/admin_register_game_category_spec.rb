3074fafafafa4ad47d11cb61913b306387ebe7cc
Vini Santana <viniksantana5@gmail.com>
2019-03-31 04:31:40 -0300

    Adding test to admin register game category successfully

---
+++ spec/features/admin_register_game_category_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'Admin register game category' do
+  scenario 'successfully' do
+    admin = create(:admin)
+
+    visit root_path
+    click_on 'Cadastrar Categoria de Jogo'
+    fill_in 'Nome', with: 'Tiro'
+    click_on 'Enviar'
+
+    expect(page).to have_content('Categoria Tiro cadastrada com sucesso!')
+  end
+end
