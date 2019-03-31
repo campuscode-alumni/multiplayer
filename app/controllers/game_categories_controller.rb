1269a203781214ac8d15c2c5a5b7f082f7597e6a
Vini Santana <viniksantana5@gmail.com>
2019-03-31 05:56:40 -0300

    Sucessful create method

--- app/controllers/game_categories_controller.rb
+++ app/controllers/game_categories_controller.rb
@@ -5,14 +5,16 @@ class GameCategoriesController < ApplicationController
 
   def create
     @new_game_category = GameCategory.new(params_category)
-    if @new_game_category.save
-      redirect_to new_game_category_path
-      flash[:notice] = "Categoria #{@new_game_category.name} \n
-                        cadastrada com sucesso!"
-    end
+    return successful_create if @new_game_category.save
   end
 
   private
+
+  def successful_create
+    redirect_to new_game_category_path
+    flash[:notice] = "Categoria #{@new_game_category.name} \n
+                        cadastrada com sucesso!"
+  end
 
   def params_category
     params.require(:game_category).permit(:name)
