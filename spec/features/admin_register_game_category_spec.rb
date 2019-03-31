7239a7c1c0a759a28adf291ee89e88faabda7068
Vini Santana <viniksantana5@gmail.com>
2019-03-31 06:00:32 -0300

    Only logged admin can see button

--- spec/features/admin_register_game_category_spec.rb
+++ spec/features/admin_register_game_category_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 feature 'Admin register game category' do
   scenario 'successfully' do
-    create(:admin)
+    admin = create(:admin)
 
+    login_as admin, scope: :admin
     visit root_path
     click_on 'Cadastrar Categoria de Jogo'
     fill_in 'Nome', with: 'Tiro'
     click_on 'Enviar'
 
     expect(page).to have_content('Categoria Tiro cadastrada com sucesso!')
+  end
+
+  scenario 'and only logged admin can see button' do
+    visit root_path
+
+    expect(page).not_to have_content('Cadastrar Categoria de Jogo')
   end
 end
