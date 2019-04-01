ea86101b99326d1957639029338c546cfb8fa795
Luis Arantes <luis.arantes.sp@gmail.com>
2019-03-31 21:43:46 -0300

    Add permission and navigation tests for companies
      do not let not authenticated admins register new companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_action :authenticate_admin!
+
   def new
     @company = Company.new
   end
--- spec/features/admin_register_company_spec.rb
+++ spec/features/admin_register_company_spec.rb
@@ -12,6 +12,7 @@ feature 'Admin register company' do
     click_on 'Enviar'
 
     expect(page).to have_content('Empresa Sega cadastrada com sucesso')
+    expect(page).not_to have_css('h2', text: 'Cadastrar Empresa Fabricante')
   end
 
   scenario 'but name is required' do
@@ -24,6 +25,7 @@ feature 'Admin register company' do
     click_on 'Enviar'
 
     expect(page).to have_content('Nome não pode ficar em branco')
+    expect(page).to have_css('h2', text: 'Cadastrar Empresa Fabricante')
   end
 
   scenario 'but name cannot be repeated' do
@@ -37,8 +39,9 @@ feature 'Admin register company' do
     click_on 'Enviar'
 
     expect(page).to have_content('Nome já está em uso')
+    expect(page).to have_css('h2', text: 'Cadastrar Empresa Fabricante')
   end
-
+  
   scenario 'but logo cannot be empty' do
     admin = create(:admin)
 
@@ -49,5 +52,28 @@ feature 'Admin register company' do
     click_on 'Enviar'
 
     expect(page).to have_content('Logo precisa ser enviado')
+    expect(page).to have_css('h2', text: 'Cadastrar Empresa Fabricante')
+  end
+  
+  scenario 'but do not see button if not authenticated' do
+    visit root_path
+
+    expect(page).not_to have_content('Cadastrar Empresa Fabricante')
+  end
+  
+  scenario 'but only if authenticated' do
+    visit new_company_path
+
+    expect(current_path).to eq new_admin_session_path
+  end
+  
+  scenario 'but gives up and return to home' do
+    admin = create(:admin)
+    
+    login_as(admin, scope: :admin)
+    visit new_company_path
+    click_on 'Voltar'
+    
+    expect(current_path).to eq root_path
   end
 end
