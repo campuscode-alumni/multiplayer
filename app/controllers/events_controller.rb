71971f388449601b93d86cdaa39224196785b747
Felipe, Luis e Vinicius <viniksantana5@gmail.com>
2019-04-04 21:56:22 -0300

    Complete user invitation tests
    Co-authored-by: Luis <luis.arantes.sp@gmail.com>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,10 +18,16 @@ class EventsController < ApplicationController
   end
 
   def invite
+    q = params[:q]
     @event = Event.find(params[:id])
-    @invited_user = User.find_by(email: params[:q]) || User.find_by(nickname: params[:q])
-    EventInvite.create(event: @event, user: current_user, invitee: @invited_user)
-    flash[:notice] = "Convite enviado para o usuário #{@invited_user.nickname}"
+    #@invited_user = User.find_by(email: q) || User.find_by(nickname: q)
+    @invited_user = User.find_by('nickname = :q OR email = :q', q: q)
+    if @invited_user
+      EventInvite.create(event: @event, user: current_user, invitee: @invited_user)
+      flash[:notice] = "Convite enviado para o usuário #{@invited_user.nickname}"
+    else
+      flash[:notice] = "Não foi encontrado um usuário com este email"
+    end
     redirect_to @event
   end
 
