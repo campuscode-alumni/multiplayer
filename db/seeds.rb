13183a0bb81e8ee2622aff9140cf56657625d648
Luis Arantes <luis.arantes.sp@gmail.com>
2019-04-09 01:02:30 -0300

    Add companies, platforms and categories seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,42 +3,85 @@ if (!Admin.find_by(email: 'admin@admin.com'))
 end
 
 if (!User.find_by(email: 'user@user.com'))
-  FactoryBot.create(:user, name: 'user', email: 'user@user.com', password: '123456')
+  User.create(name: 'user', email: 'user@user.com', password: '123456')
 end
 
 if (!City.find_by(name: 'São Paulo'))
   state = State.find_by(name: 'SP')
   if state.nil?
-    state = FactoryBot.create(:state, name: 'SP')
+    state = State.create(name: 'SP')
   end
-  FactoryBot.create(:city, name: 'São Paulo', state: state)
+  City.create(name: 'São Paulo', state: state)
 end
 
 # empresas
+
 if (!Company.find_by(name: 'Nintendo'))
   c = Company.new(name: 'Nintendo')
   c.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'nintendo.png')), filename: 'nintendo.png')
-  c.save  
+  c.save
 end
 
 if (!Company.find_by(name: 'Sega'))
   c = Company.new(name: 'Sega')
   c.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'sega.png')), filename: 'sega.png')
-  c.save  
+  c.save
 end
 
 if (!Company.find_by(name: 'Sony'))
   c = Company.new(name: 'Sony')
   c.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'sony.png')), filename: 'sony.png')
-  c.save  
+  c.save
 end
 
 if (!Company.find_by(name: 'Microsoft'))
   c = Company.new(name: 'Microsoft')
   c.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'microsoft.png')), filename: 'microsoft.png')
-  c.save  
+  c.save
 end
 
-# todo plataformas
+# plataformas
 
-# todo ganes
+if (!Platform.find_by(name: 'Super Nintendo'))
+  p = Platform.new(name: 'Super Nintendo', company: Company.find_by(name: 'Nintendo'))
+  p.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'snes.png')), filename: 'snes.png')
+  p.save
+end
+
+if (!Platform.find_by(name: 'Mega Drive'))
+  p = Platform.new(name: 'Mega Drive', company: Company.find_by(name: 'Sega'))
+  p.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'megadrive.png')), filename: 'megadrive.png')
+  p.save
+end
+
+if (!Platform.find_by(name: 'Nintendo Switch'))
+  p = Platform.new(name: 'Nintendo Switch', company: Company.find_by(name: 'Nintendo'))
+  p.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'switch.png')), filename: 'switch.png')
+  p.save
+end
+
+if (!Platform.find_by(name: 'Playstation 4'))
+  p = Platform.new(name: 'Playstation 4', company: Company.find_by(name: 'Sony'))
+  p.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'ps4.png')), filename: 'ps4.png')
+  p.save
+end
+
+if (!Platform.find_by(name: 'X Box One'))
+  p = Platform.new(name: 'X Box One', company: Company.find_by(name: 'Microsoft'))
+  p.logo.attach(io: File.open(Rails.root.join('spec', 'support', 'xone.png')), filename: 'xone.png')
+  p.save
+end
+
+# categorias
+
+def create_category(name)
+  if !Category.find_by(name: name)
+    Category.create(name: name)
+  end
+end
+
+categories = ['RPG', 'FPS', 'Plataforma', 'Esporte', 'Aventura']
+categories.each { |c| create_category(c) }
+
+# todo games
+
--- spec/factories/platforms.rb
+++ spec/factories/platforms.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :platform do
     sequence(:name) { |i| "Plataforma #{i}" }
+    company
     logo do
       fixture_file_upload(
         Rails.root.join('spec', 'support', 'ps4.png'), 'image/png'
