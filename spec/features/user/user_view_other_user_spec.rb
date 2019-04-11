941fe94c453175c86c77234f045be717ada12be6
Felipe e Luis <luis.arantes.sp@gmail.com>
2019-04-10 21:49:51 -0300

    Fix Rubocop

--- spec/features/user/user_view_other_user_spec.rb
+++ spec/features/user/user_view_other_user_spec.rb
@@ -1,24 +1,16 @@
 require 'rails_helper'
 
 feature 'User View users profiles' do
-
   scenario 'view own profile' do
     user = create(:user)
-    
     login_as(user, scope: :user)
-
     visit user_path(user)
-  
-    expect(page).not_to have_css('div', id:'invite_to_event')
-    
+    expect(page).not_to have_css('div', id: 'invite_to_event')
   end
 
   scenario 'does not view profiles if not signed in' do
     user = create(:user)
-
     visit user_path(user)
-  
     expect(current_path).to eq new_user_session_path
-    
   end
 end
